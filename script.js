940d93f445d2367c7164222871397fcdee684aa5
kamilimaier-alt <kamili.maier@escola.pr.gov.br>
2026-08-03 11:37:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-function abrirHistoria(){
-    document.getElementById("janelaHistoria").style.display = "block";
+// Seleciona os elementos do HTML
+const modal = document.getElementById('meuModal');
+const btnAbrir = document.getElementById('btnAbrirModal');
+const btnFechar = document.querySelector('.fechar');
+
+// Abre a janela ao clicar no botão
+btnAbrir.onclick = function() {
+  modal.style.display = 'block';
 }
 
-function fecharHistoria(){
-    document.getElementById("janelaHistoria").style.display = "none";
+// Fecha a janela ao clicar no (X)
+btnFechar.onclick = function() {
+  modal.style.display = 'none';
+}
+
+// Fecha a janela se o usuário clicar fora do conteúdo
+window.onclick = function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
 }
